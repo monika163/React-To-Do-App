97aef966f6683d04444f14ff6ace14d4f8c265e8
MDEWANGA <monika3pageit@gmail.com>
2022-08-17 15:53:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Tabs from "./component/TabMenu/Tabs";
+
+// import GalleryReact from "./component/TabMenu/GalleryReact";
+import Todo from "./component/todoProject/todo.js";
+
+const App = () => {
+  return (
+    <>
+     {/* <GalleryReact /> */}
+     <Todo />
+    </>
+  )
+}
+
+export default App
